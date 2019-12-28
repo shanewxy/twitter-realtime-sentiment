ef8340e25ee3d82b524a2516ddf1e12e2fa0304f
kedi <904228465@qq.com>
2019-12-28 13:03:03 +1100

    store tweets to couchdb(localhost:5984)

--- data_collection/twitter_streaming.py
+++ data_collection/twitter_streaming.py
@@ -4,10 +4,20 @@ from tweepy import Stream
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
+import couchdb
 
 import twitter_credentials
 
 analyser = SentimentIntensityAnalyzer()
+
+db_user = "admin"
+db_pwd = "admin"
+db_server = couchdb.Server("http://%s:%s@localhost:5984/" % (db_user, db_pwd))
+db_name = "twitter_realtime_sentiment"
+if db_name in db_server:
+    db = db_server[db_name]
+else:
+    db = db_server.create(db_name)
 
 
 class TwitterStreamer:
@@ -33,12 +43,23 @@ class MyStreamListener(StreamListener):
         try:
             data_object = json.loads(data)
             store_tweets_locally(filename, data_object)
+            store_tweets_db(data_object)
 
         except BaseException as e:
             print("Error on data: %s " % str(e))
 
     def on_error(self, status_code):
         print(status_code)
+
+
+def store_tweets_db(data_object):
+    doc_id, doc_rev = db.save({'text': data_object['text'],
+                               'coordinates': data_object['coordinates'],
+                               'created_at': data_object['created_at'],
+                               'place': data_object['place']['name'],
+                               'user_id': data_object['user']['id'],
+                               'user_location': data_object['user']['location'],
+                               'sentiment': sentiment_analyzer_scores(data_object['text'])})
 
 
 def store_tweets_locally(filename, data_object):
@@ -49,12 +70,12 @@ def store_tweets_locally(filename, data_object):
     :return:
     """
     db_tweets = dict()
-    db_tweets['id'] = data_object['id']
     db_tweets['text'] = data_object['text']
     db_tweets['coordinates'] = data_object['coordinates']
     db_tweets['created_at'] = data_object['created_at']
-    db_tweets['user_id'] = data_object['user']['id']
     db_tweets['place'] = data_object['place']['name']
+    db_tweets['user_id'] = data_object['user']['id']
+    db_tweets['user_location'] = data_object['user']['location']
     # db_tweets['sentiment'] = TextBlob(data_object['text']).sentiment.polarity
     db_tweets['sentiment'] = sentiment_analyzer_scores(data_object['text'])
 
