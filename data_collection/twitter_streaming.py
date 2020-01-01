4870c38413bee37757fdfd9f431297cb3b9ce56f
kedi <904228465@qq.com>
2020-01-01 13:49:01 +1100

    remove 'compound' field

--- data_collection/twitter_streaming.py
+++ data_collection/twitter_streaming.py
@@ -6,18 +6,16 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
 import couchdb
 import argparse
-
 import twitter_credentials
 
-analyser = SentimentIntensityAnalyzer()
 
 class TwitterStreamer:
     """
     streaming live tweets
     """
 
-    def stream_tweets(self, filename, locations):
-        listener = MyStreamListener(filename)
+    def stream_tweets(self, locations):
+        listener = MyStreamListener()
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 
@@ -27,12 +25,8 @@ class TwitterStreamer:
 
 class MyStreamListener(StreamListener):
 
-    def __init__(self, filename):
-        self.filename = filename
-
     def on_data(self, data):
         try:
-            # print(data)
             data_object = json.loads(data)
             # store_tweets_locally(filename, data_object)
             store_tweets_db(data_object)
@@ -45,13 +39,18 @@ class MyStreamListener(StreamListener):
 
 
 def store_tweets_db(data_object):
-    doc_id, doc_rev = db.save({'text': data_object['text'],
-                               'coordinates': data_object['coordinates'],
-                               'created_at': data_object['created_at'],
-                               'place': data_object['place']['name'],
-                               'user_id': data_object['user']['id'],
-                               'user_location': data_object['user']['location'],
-                               'sentiment': sentiment_analyzer_scores(data_object['text'])})
+    place = data_object['place']['name']
+
+    # double check place here, no idea why Streaming API will return 'New South Wales' or 'Victoria'
+    # even if already set the stream filter
+    if str(place) != 'New South Wales' and str(place) != 'Victoria':
+        doc_id, doc_rev = db.save({'text': data_object['text'],
+                                   'coordinates': data_object['coordinates'],
+                                   'created_at': data_object['created_at'],
+                                   'place': data_object['place']['name'],
+                                   'user_id': data_object['user']['id'],
+                                   'user_location': data_object['user']['location'],
+                                   'sentiment': sentiment_analyzer_scores(data_object['text'])})
 
 
 def store_tweets_locally(filename, data_object):
@@ -80,21 +79,21 @@ def store_tweets_locally(filename, data_object):
 
 def sentiment_analyzer_scores(text):
     """
-    Get sentiment score using Vader
-    :param text:
-    :return:
+    Get sentiment score using Vader, normalize sentiment score to [-1, 1]
+    :param text: The sentence to be analyzed
+    :return: The sentiment score
     """
-    score = analyser.polarity_scores(text)
+    score = SentimentIntensityAnalyzer().polarity_scores(text)
+    compound = score['compound']
 
-    # if score['compound'] >= 0.05:
-    #     sentiment = score['pos']
-    # elif score['compound'] <= -0.05:
-    #     sentiment = -score['neg']
-    # else:
-    #     sentiment = score['neu']
+    if -0.05 < compound < 0.05:
+        sentiment = 0
+    elif compound >= 0.05:
+        sentiment = score['pos']
+    elif compound <= -0.05:
+        sentiment = -score['neu']
 
-    # print("{:-<40} {}".format(text, str(score)))
-    return score
+    return sentiment
 
 
 if __name__ == "__main__":
@@ -103,12 +102,13 @@ if __name__ == "__main__":
     filename = "tweets.json"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--user", default="admin", type=str, help="couchdb user")
-    parser.add_argument("--pwd", default="admin", type=str, help="couchdb password")
-    parser.add_argument("--server", default="localhost:5984", type=str, help="couchdb server address")
+    # parser.add_argument("--user", default="admin", type=str, help="couchdb user")
+    # parser.add_argument("--pwd", default="admin", type=str, help="couchdb password")
+    parser.add_argument("--server", default="127.0.0.1:5984", type=str, help="couchdb server address")
     args = parser.parse_args()
 
-    db_server = couchdb.Server("http://%s:%s@%s/" % (args.user, args.pwd, args.server))
+    # db_server = couchdb.Server("http://%s:%s@%s/" % (args.user, args.pwd, args.server))
+    db_server = couchdb.Server("http://%s/" % args.server)
     db_name = "twitter_realtime_sentiment"
     if db_name in db_server:
         db = db_server[db_name]
@@ -116,4 +116,4 @@ if __name__ == "__main__":
         db = db_server.create(db_name)
 
     twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(filename, locations)
+    twitter_streamer.stream_tweets(locations)
