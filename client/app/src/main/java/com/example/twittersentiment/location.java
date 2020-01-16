44d137c41094d575f504afa28918c79dd5193029
yanwu20 <13671010156@163.com>
2020-01-16 15:18:46 +1100

    add location choice

--- client/app/src/main/java/com/example/twittersentiment/location.java
+++ client/app/src/main/java/com/example/twittersentiment/location.java
@@ -2,6 +2,7 @@ package com.example.twittersentiment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -10,6 +11,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Camera;
 import android.graphics.Color;
 import android.location.Address;
@@ -23,8 +25,12 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -61,7 +67,7 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
 
-public class location extends FragmentActivity implements OnMapReadyCallback,
+public class location extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnMarkerClickListener, LocationListener {
 
@@ -73,6 +79,7 @@ public class location extends FragmentActivity implements OnMapReadyCallback,
     private String TAG = "location";
     private Button search;
     private EditText time;
+    private AutoCompleteTextView sa2Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +91,18 @@ public class location extends FragmentActivity implements OnMapReadyCallback,
         time = (EditText) findViewById(R.id.Time);
         String timeString = time.getText().toString();
         search = findViewById(R.id.button);
+        sa2Name = findViewById(R.id.locationName);
+
+        // the autoCompeleteTextView
+        Resources resources = getResources();
+        String[] name = resources.getStringArray(R.array.sa2NameArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,name);
+        sa2Name.setAdapter(adapter);
+
+        // obtain the input of the SA2 name
+//        sa2Name.getOnItemSelectedListener()
+
+
 
         search.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
@@ -95,10 +114,16 @@ public class location extends FragmentActivity implements OnMapReadyCallback,
                     minute = 600;
                 }
                 Intent intent = new Intent(location.this,result.class);
-                intent.putExtra("suburb", suburbName);
+                if (!sa2Name.getText().toString().equals("")){
+                    String enteredName = sa2Name.getText().toString();
+                    String enteredSuburbName = getQueryName(enteredName);
+                    intent.putExtra("suburb",enteredSuburbName);
+                }
+                else{
+                    intent.putExtra("suburb", suburbName);
+                }
                 intent.putExtra("minute",minute);
                 startActivity(intent);
-//                sendHttpRequest();
 
             }
         });
@@ -171,130 +196,6 @@ public class location extends FragmentActivity implements OnMapReadyCallback,
         }
     }
 
-
-    public  void sendHttpRequest(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int minute;
-                    if(!time.getText().toString().equals("")) {
-                        minute = Integer.parseInt(time.getText().toString());
-                    }
-                    else {
-                        minute = 600;
-                    }
-                    URL historyURL = new URL("http://1926b0aa.jp.ngrok.io/stats/historic");
-                    URL realURL = new URL("http://1926b0aa.jp.ngrok.io/stats/realtime?minute="+minute);
-                    URL topicURL = new URL("http://1926b0aa.jp.ngrok.io/stats/realtime/topics?minute="+minute);
-                    JSONObject his_result =  getHttpConnection(historyURL);
-                    JSONObject real_result  = getHttpConnection(realURL);
-                    JSONObject topic_result = getHttpConnection(topicURL);
-                    showResponse(his_result,real_result);
-                    Log.d(TAG, "run: get the http result"+ his_result.getString("('Alphington - Fairfield', '21110')"));
-                    Log.d(TAG, "run: "+ real_result.toString());
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "run: "+e.getMessage());
-                }
-            }
-        }).start();
-    }
-
-    private JSONObject getHttpConnection(URL url){
-        try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            int responseCode = connection.getResponseCode();
-            if(responseCode == HttpURLConnection.HTTP_OK  ){
-                InputStream input = connection.getInputStream();
-                JSONObject result =  getJsonObject(input);
-                connection.disconnect();
-                return result;
-            }
-            else{
-                networkError();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-
-    }
-
-    private void showResponse(final JSONObject his_result, final JSONObject real_result) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    GeoJsonLayer layer = new GeoJsonLayer(mMap, R.raw.boundary,
-                            getApplicationContext());
-                    layer.addLayerToMap();
-                    layer.getFeatures();
-                    layer.setOnFeatureClickListener(new GeoJsonLayer.GeoJsonOnFeatureClickListener() {
-                        @Override
-                        public void onFeatureClick(Feature feature) {
-                            GeoJsonLineStringStyle lineStringStyle = new GeoJsonLineStringStyle();
-                            GeoJsonFeature lineStringFeature = (GeoJsonFeature) feature;
-                            lineStringStyle.setColor(Color.RED);
-                            lineStringStyle.setZIndex(10f);
-                            lineStringStyle.setWidth(2f);
-                            lineStringFeature.setLineStringStyle(lineStringStyle);
-                            Log.d(TAG, "onFeatureClick: ");
-
-                        }
-                    });
-                    Log.d(TAG, "run: showResponse");
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void networkError(){
-        runOnUiThread((new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(location.this,"Network Error",Toast.LENGTH_SHORT);
-            }
-        }));
-    }
-
-
-
-
-    //Returns a json object from an input stream
-    private JSONObject getJsonObject(InputStream input){
-
-        try {
-            BufferedReader streamReader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
-            StringBuilder responseStrBuilder = new StringBuilder();
-            String inputStr;
-            while ((inputStr = streamReader.readLine()) != null)
-                responseStrBuilder.append(inputStr);
-
-            JSONObject jsonObject = new JSONObject(responseStrBuilder.toString());
-
-            //returns the json object
-            return jsonObject;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        //if something went wrong, return null
-        return null;
-    }
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -321,6 +222,7 @@ public class location extends FragmentActivity implements OnMapReadyCallback,
         }
     }
 
+    // convert the location to suburb name and SA2 code
     public String getSuburb(Location location) throws IOException {
         Geocoder addressCoder = new Geocoder(this);
         List address = addressCoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
@@ -344,6 +246,27 @@ public class location extends FragmentActivity implements OnMapReadyCallback,
 
         return  null;
     }
+
+    public String getQueryName(String enteredName){
+        try {
+            InputStreamReader is = new InputStreamReader(getResources().openRawResource(R.raw.code_sa2));
+            BufferedReader reader = new BufferedReader(is);
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String item[] = line.split(",");
+                if(enteredName.equalsIgnoreCase(item[1])){
+                    return "('"+item[1]+"', '"+item[0]+"')";
+                }
+            }
+        } catch (IOException e) {
+            Log.d(TAG, "getSuburb: "+e.getMessage());
+        }
+
+        return  null;
+
+    }
+
 
 
 
